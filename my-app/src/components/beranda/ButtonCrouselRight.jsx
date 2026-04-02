0c7b28fc2dfb9b081f0a12cf9304c54b4d4d3893
RoihanTauhid <roihanntauhidd@gmail.com>
2026-04-03 02:36:41 +0700

    Crousel

---
+++ my-app/src/components/beranda/ButtonCrouselRight.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style/ButtonCrousel.css"
+
+function ButtonCrouselRight() {
+  return (
+    <button id="ButtonCrouselRight" class="ButtonCrouselRight"> &lt; </button>
+  )
+}
+
+export default ButtonCrouselRight
---
+++ my-app/src/components/beranda/CardLandscape.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ButtonCrouselLeft from './ButtonCrouselLeft'
+import ButtonCrouselRight from './ButtonCrouselRight'
+
+function CardLandscape() {
+  return (
+    <div className='CardLandscape'>
+        <h1>Melanjutkan Tonton Film</h1>
+        <div className='crousel'>
+            <ButtonCrouselLeft />
+            
+        </div>
+    </div>
+  )
+}
+
+export default CardLandscape
